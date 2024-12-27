af25dacd74c4e1538dd5ff81271556e6fe157b5a
Eylon Fayena <eylonf8@gmail.com>
2024-12-27 15:32:55 +0200

    Create the main screen of the app for the list of students

--- app/src/main/java/com/efai/studentsapp/MainActivity.kt
+++ app/src/main/java/com/efai/studentsapp/MainActivity.kt
@@ -1,47 +1,22 @@
 package com.efai.studentsapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.efai.studentsapp.ui.theme.StudentsAppTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var studentsListRecyclerView: RecyclerView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            StudentsAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StudentsAppTheme {
-        Greeting("Android")
+        studentsListRecyclerView = findViewById(R.id.studentList)
+        studentsListRecyclerView.layoutManager = LinearLayoutManager(this)
     }
+
 }
