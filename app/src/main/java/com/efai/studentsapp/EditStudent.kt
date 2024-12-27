578de33999c9c53db07b397864b79d0024f271e8
adir <adir@gmail.com>
2024-12-27 17:10:14 +0200

    Add editStudent module and xml, setups edit listenner in viewStudent

---
+++ app/src/main/java/com/efai/studentsapp/EditStudent.kt
@@ -0,0 +1,81 @@
+package com.efai.studentsapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class EditStudent : AppCompatActivity() {
+
+    private var position: Int = 0
+    private lateinit var nameTextView: TextView
+    private lateinit var idTextView: TextView
+    private lateinit var phoneTextView: TextView
+    private lateinit var addressTextView: TextView
+    private lateinit var checkedCheckBox: CheckBox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_student)
+
+        nameTextView = findViewById(R.id.editName)
+        idTextView = findViewById(R.id.editId)
+        phoneTextView = findViewById(R.id.editPhone)
+        addressTextView = findViewById(R.id.editAddress)
+        checkedCheckBox = findViewById(R.id.editChecked)
+
+        intent?.let {
+            nameTextView.text = it.getStringExtra("name")
+            idTextView.text = it.getStringExtra("id")
+            phoneTextView.text = it.getStringExtra("phone")
+            addressTextView.text = it.getStringExtra("address")
+            checkedCheckBox.isChecked = it.getBooleanExtra("checked", false)
+            position = it.getIntExtra("position", 0)
+        }
+
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        val saveButton: Button = findViewById(R.id.editSave)
+        val cancelButton: Button = findViewById(R.id.editCancel)
+        val deleteButton: Button = findViewById(R.id.editDelete)
+
+        saveButton.setOnClickListener {
+            editStudent()
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        cancelButton.setOnClickListener {
+            finish()
+        }
+
+        deleteButton.setOnClickListener {
+            MainActivity.studentsList.removeAt(position)
+            MainActivity.studentsAdapter.notifyItemRemoved(position)
+            // Return to MainActivity and clear back stack
+            val intent = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            startActivity(intent)
+            // Ensure the current activity is finished
+            finish()
+        }
+    }
+
+    private fun editStudent() {
+        MainActivity.studentsList[position].apply {
+            setStudent(
+                nameTextView.text.toString(),
+                idTextView.text.toString(),
+                phoneTextView.text.toString(),
+                addressTextView.text.toString(),
+                checkedCheckBox.isChecked
+            )
+        }
+        MainActivity.studentsAdapter.notifyItemChanged(position)
+    }
+}
